3f709b19cc4b63c77dd3f1b5dd3f405a3fa2a086
Ken.Lewis <19KennethL89@gmail.com>
2025-08-01 20:22:33 -0400

    Sprint 7 1st Submission

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Simple Todo App
 
-Give a brief description of the project here. Feel free to give it a different name.
+A simple user-friendly todo application. Users can create new todo items that will be displayed in a list.
 
 ## Functionality
 
-Give a more detailed explanation of the project and its functionality.
+On top of users being able to create new todo items, users can also set a date for when they would like their tasks to be completed. Task can also be deleted before they are completed. Users can also use checkboxes to mark specific tasks that need to be completed or are already completed.
 
 ## Technology
 
-Give a description of the technologies and techniques used. Pictures, GIFs, or screenshots that detail the project features are recommended.
+HTML, CSS, JavaScript are the languages used to build this application. Javascript was used a lot during the making of this application. The JavaScript used gives users the ability to create and delete todo items, event listeners, error messages, form validation, open and closing modals and working delete and close buttons. The HTML was used for the layout and the CSS was used to approve the appearance of the todo application.
 
 ## Deployment
 
---
+++ components/FormValidator.js
@@ -0,0 +1,88 @@
+class FormValidator {
+  constructor(settings, formEl) {
+    this._inputSelector = settings.inputSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._errorClass = settings.errorClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
+    this._formEl = formEl;
+    this._buttonElement = this._formEl.querySelector(
+      this._submitButtonSelector
+    );
+  }
+
+  _showInputError(inputElement, errorMessage) {
+    const errorElement = this._formEl.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.classList.add(this._errorClass);
+    errorElement.textContent = inputElement.validationMessage;
+  }
+
+  _hideInputError(inputElement) {
+    const errorElement = this._formEl.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = "";
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _toggleButtonState() {
+    const hasInvalidInput = this._inputList.some(
+      (input) => !input.validity.valid
+    );
+
+    if (hasInvalidInput) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.disabled = false;
+    }
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
+    const buttonElement = this._formEl.querySelector(
+      this._submitButtonSelector
+    );
+
+    this._toggleButtonState();
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+
+  resetValidation() {
+    this._inputList.forEach((inputElement) => {
+      inputElement.classList.remove(this._inputErrorClass);
+    });
+    this._formEl.reset();
+    this._buttonElement.disabled = true;
+  }
+}
+
+export default FormValidator;
